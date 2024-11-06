d787d12bca786ea9be216d0d6087a477273aded8
akpaz <akpaz@udesa.edu.ar>
2024-11-06 00:15:01 -0300

    arranque con el menu de navegacion y login

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Register" component={Register}/>
+      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Profile" component={Profile}/>
+        <Tab.Screen name="Posts" component={Posts}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
